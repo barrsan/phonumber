cbe0c88043017721343f0e4d0cb8bf28135a4d5e
Alex Baretsky <41ax@mail.ru>
2022-08-30 22:52:06 +0400

    Use es format

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'phonumber',
+      formats: ['es'],
       fileName: 'phonumber',
     },
   },
+  plugins: [dts()],
 });
